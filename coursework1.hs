9221e2b6c8371ba092de044c91a0161c2d382d73
hjunger <HenriqueJunger@Henriques-MacBook-Pro.local>
2015-11-04 10:48:43 +0000

    Lendo pro Haskell

--- coursework1.hs
+++ coursework1.hs
@@ -1,19 +1,74 @@
 --import Entites
+import Data.Char
 
 j = readFile "userprofile.json"
 
-
 data User = User {
-					_Id::String
+					id::String,
+					index::Int, 
+					isActive::Bool,
+					balance::String,
+					picture::String, 
+					age::Int,
+					eyeColor::String,
+					--name::Name,
+					company::String,
+					email::String
+					--phone::String,
+					--address::String,
+					--registered::String,
+					--latitude::String,
+					--longitude::String,
+					--friends::[Friend],
+					--greeting::String,
+					--favoriteFruit::String
 				}deriving(Show)
 
 
+trim::String->String
+trim xs = dropSpaceTail "" $ dropWhile isSpace xs
+
+dropSpaceTail::String->String->String
+dropSpaceTail maybeStuff "" = ""
+dropSpaceTail maybeStuff (x:xs)
+        | isSpace x = dropSpaceTail (x:maybeStuff) xs
+        | null maybeStuff = x : dropSpaceTail "" xs
+        | otherwise       = reverse maybeStuff ++ x : dropSpaceTail "" xs
+
+capitalFst::String->String
+capitalFst s = toUpper (head s):tail s
+
 -- js <- j
--- let json = lines js
--- let begin = "    \"_id\": "
--- let test = getSomeInfo (getBeginWith json begin) begin ','
-isID::String->Bool
-isID s = (take 3 s)=="_id"
+-- let json = removeCharList (trimJson (lines js)) '\"'
+-- let id = getSomeInfo (getBeginWith json "_id:") "_id: " ','
+-- let indexStr = getSomeInfo (getBeginWith json "index: ") "index: " ','
+-- let index = read indexStr :: Int
+-- let isActiveStr = capitalFst (getSomeInfo (getBeginWith json "isActive: ") "isActive: " ',')
+-- let isActive = read isActiveStr :: Bool
+-- let balance = getSomeInfo (getBeginWith json "picture: ") "picture: " ','
+-- let picture = getSomeInfo (getBeginWith json "age: ") "age: " ','
+-- let ageStr = getSomeInfo (getBeginWith json "age: ") "age: " ','
+-- let age = read ageStr :: Int
+-- let eyeColor = getSomeInfo (getBeginWith json "eyeColor: ") "eyeColor: " ','
+-- let name
+-- let company = getSomeInfo (getBeginWith json "eyeColor: ") "eyeColor: " ','
+-- let email = getSomeInfo (getBeginWith json "email: ") "email: " ','
+-- createUser id index isActive balance picture age eyeColor company email
+
+
+trimJson::[String]->[String]
+trimJson [] = []
+trimJson (x:xs) = trim x : trimJson xs
+
+removeChar::String->Char->String
+removeChar "" _ = ""
+removeChar (x:xs) c = if x == c 
+						then removeChar xs c
+						else x : (removeChar xs c)
+
+removeCharList::[String]->Char->[String]
+removeCharList [] _ = []
+removeCharList (x:xs) c = (removeChar x c) : removeCharList xs c
 
 strip::String->String->String
 strip "" _ = ""
@@ -33,5 +88,7 @@ getSomeInfo::String->String->Char->String
 getSomeInfo "" _ _ = ""
 getSomeInfo sth stComp chFin = let st = strip sth stComp in take (myIndex st chFin) st
 
-createUser::String->User
-createUser s = User s
+createUser::String->Int->Bool->String->String->Int->String->String->String->User
+createUser i idx act bal pic ag ec com em = User i idx act bal pic ag ec com em
+
+

c630588030b6147212a5e06a1b3c5104faba5062
hjunger <HenriqueJunger@Henriques-MacBook-Pro.local>
2015-11-06 16:11:27 +0000

    henrique

--- coursework1.hs
+++ coursework1.hs
@@ -2,7 +2,7 @@
 -- Group: Victor Benning and Henrique Junger
 
 -- To generate the XML, just run the follow command:
--- processFile processJson "userprofile.json" "teste.xml"
+-- convertJsonToXml "userprofile.json" "teste.xml"
 
 --import Entites
 
@@ -10,40 +10,40 @@
 import Data.Char
 import Data.List
 
-j = readFile "userprofile.json"
+--j = readFile "userprofile.json"
 
 -- Data Types
 data Name = Name {
-				firstName::String,
-				lastName::String
-			}deriving(Show)
+                firstName::String,
+                lastName::String
+            }deriving(Show)
 
 data Friend = Friend {
-				idFriend::Int,
-				nameFriend::String
-			}deriving(Show)
+                idFriend::Int,
+                nameFriend::String
+            }deriving(Show)
 
 data User = User {
-					_id::String,
-					index::Int, 
-					isActive::Bool,
-					balance::String,
-					picture::String, 
-					age::Int,
-					eyeColor::String,
-					name::Name,
-					company::String,
-					email::String,
-					phone::String,
-					address::String,
-					about::String,
-					registered::String,
-					latitude::String,
-					longitude::String,
-					friends::[Friend],
-					greeting::String,
-					favoriteFruit::String
-				}deriving(Show)
+                    _id::String,
+                    index::Int, 
+                    isActive::Bool,
+                    balance::String,
+                    picture::String, 
+                    age::Int,
+                    eyeColor::String,
+                    name::Name,
+                    company::String,
+                    email::String,
+                    phone::String,
+                    address::String,
+                    about::String,
+                    registered::String,
+                    latitude::String,
+                    longitude::String,
+                    friends::[Friend],
+                    greeting::String,
+                    favoriteFruit::String
+                }deriving(Show)
 
 -- Common Functions
 trim::String->String
@@ -73,8 +73,8 @@ trimJson (x:xs) = trim x : trimJson xs
 removeChar::String->Char->String
 removeChar "" _ = ""
 removeChar (x:xs) c = if x == c 
-						then removeChar xs c
-						else x : (removeChar xs c)
+                        then removeChar xs c
+                        else x : (removeChar xs c)
 
 removeCharList::[String]->Char->[String]
 removeCharList [] _ = []
@@ -111,8 +111,8 @@ getLastName (Name _ lastName) = lastName
 
 getFriends::[Friend]->String
 getFriends [] = ""
-getFriends (x:xs) = "<friend> <id>" ++ show (idFriend x) ++ 
-					"</id> "++ "<name>"++ nameFriend x ++ "</name> </friend>" ++ getFriends(xs)
+getFriends (x:xs) = "\t\t<friend>\n\t\t\t<id>" ++ show (idFriend x) ++ 
+                    "</id>\n"++ "\t\t\t<name>"++ nameFriend x ++ "</name>\n\r\t\t</friend>\n" ++ getFriends(xs)
 
 listBreak :: (a -> Bool) -> [a] -> [[a]]
 listBreak pred [] = []
@@ -127,10 +127,10 @@ isBegin x = (take 1 x)=="{" && (last x)/=','
 concatClasses::[String]->[String]
 concatClasses [] = []
 concatClasses (x:xs) = if (isInfixOf "[" x)&&((length x) > 1) 
-						then 
-							let idx = (myIndex (x:xs) "],") + 1
-							in ((myConcat (take idx (x:xs))) : concatClasses (drop idx (x:xs)))
-						else x : concatClasses xs
+                        then 
+                            let idx = (myIndex (x:xs) "],") + 1
+                            in ((myConcat (take idx (x:xs))) : concatClasses (drop idx (x:xs)))
+                        else x : concatClasses xs
 
 -- Functions to create the Data types
 -- Example: "{id: 1,name: Rafael Nadal},{id: 2,name: Novak Djokovic},{id: 3,name: Roger Federer}"
@@ -138,117 +138,128 @@ createFriends::String->[Friend]
 createFriends "" = []
 createFriends "]," = []
 createFriends s = 
-			do
-				let idx = myIndex s '}' + 1
-				let friend = take idx s
-				let friendWithoutBra = getSomeInfo friend "id: " ','
-				let idFriend = read (take (myIndex friendWithoutBra ',') friendWithoutBra) :: Int
-				let nameFriend = getSomeInfo friend "name: " '}'
-				createFriend idFriend nameFriend : createFriends (drop idx s)
+            do
+                let idx = myIndex s '}' + 1
+                let friend = take idx s
+                let friendWithoutBra = getSomeInfo friend "id: " ','
+                let idFriend = read (take (myIndex friendWithoutBra ',') friendWithoutBra) :: Int
+                let nameFriend = getSomeInfo friend "name: " '}'
+                createFriend idFriend nameFriend : createFriends (drop idx s)
 
 
 createFriend::Int->String->Friend
 createFriend ident name =
-	Friend {
-			idFriend = ident,
-			nameFriend = name
-		}
+    Friend {
+            idFriend = ident,
+            nameFriend = name
+        }
 
 createName::String->String->Name
 createName firstName lastName = 
-	Name {
-			firstName = firstName,
-			lastName = lastName
-		}
+    Name {
+            firstName = firstName,
+            lastName = lastName
+        }
 
 createUser::String->Int->Bool->String->String->Int->String->Name->String->String->String->String->String->String->String->String->[Friend]->String->String->User
 createUser i idx act bal pic ag ec n com em ph add abt reg lat lon friends gree fav = 
-	User {
-			_id = i,
-			index = idx,
-			isActive = act,
-			balance = bal,
-			picture = pic,
-			age = ag,
-			eyeColor = ec,
-			name = n,
-			company = com,
-			email = em,
-			phone = ph,
-			address = add,
-			about = abt,
-			registered = reg,
-			latitude = lat,
-			longitude = lon,
-			friends = friends,
-			greeting = gree,
-			favoriteFruit = fav
-		}
+    User {
+            _id = i,
+            index = idx,
+            isActive = act,
+            balance = bal,
+            picture = pic,
+            age = ag,
+            eyeColor = ec,
+            name = n,
+            company = com,
+            email = em,
+            phone = ph,
+            address = add,
+            about = abt,
+            registered = reg,
+            latitude = lat,
+            longitude = lon,
+            friends = friends,
+            greeting = gree,
+            favoriteFruit = fav
+        }
 
 -- Functions to process the JSON
-processFile :: (String -> String) -> FilePath -> FilePath -> IO ()
-processFile process infile outfile = 
+convertJsonToXml :: FilePath -> FilePath -> String
+convertJsonToXml infile outfile = 
   do 
     inf <- readFile infile
-    writeFile outfile (process inf)
+    json <- getContents inf 
+    processJson json
+    --writeFile outfile (processJson inf)
 
+{-
+getAvaregeOfFriends:: (Fractional a) => [User]-> a
+getAvaregeOfFriends [] = 0
+getAvaregeOfFriends users = (totalOfFriends users) / genericLength users
+
+totalOfFriends::(Fractional a)=>[User]->a
+totalOfFriends [] = 0
+totalOfFriends (u:us) = realToFrac (length (friends u) + totalOfFriends us)
+-}
 processJson::String->String
 processJson json = 
-		do
-			let jsonList = concatClasses (removeCharList (trimJson (lines json)) '\"')
-			let jsonListBreak = listBreak isBegin jsonList
-			let users = processJsonList jsonListBreak
-			haskellToXml users
+        do
+            let jsonList = concatClasses (removeCharList (trimJson (lines json)) '\"')
+            let jsonListBreak = listBreak isBegin jsonList
+            let users = processJsonList jsonListBreak
+            haskellToXml users
 
 processJsonList::[[String]]->[User]
 processJsonList [] = []
 processJsonList (x:xs) = if (head x)=="{" 
-						then (jsonToUser x) : processJsonList xs
-						else processJsonList xs
+                        then (jsonToUser x) : processJsonList xs
+                        else processJsonList xs
 
 jsonToUser::[String]->User
 jsonToUser json = 
-	do
-		let id = getSomeInfo (getBeginWith json "_id:") "_id: " ','
-		let index = read (getSomeInfo (getBeginWith json "index: ") "index: " ',') :: Int
-		let isActive = read (capitalFst (getSomeInfo (getBeginWith json "isActive: ") "isActive: " ',')) :: Bool
-		let balance = getSomeInfo (getBeginWith json "balance: ") "balance: " ','
-		let picture = getSomeInfo (getBeginWith json "picture: ") "picture: " ','
-		let age = read (getSomeInfo (getBeginWith json "age: ") "age: " ',') :: Int
-		let eyeColor = getSomeInfo (getBeginWith json "eyeColor: ") "eyeColor: " ','
-		let name = createName (getSomeInfo (getBeginWith json "first: ") "first: " ',') (getSomeInfo (getBeginWith json "last: ") "last: " ',')
-		let company = getSomeInfo (getBeginWith json "company: ") "company: " ','
-		let email = getSomeInfo (getBeginWith json "email: ") "email: " ','
-		let phone = getSomeInfo (getBeginWith json "phone: ") "phone: " ','
-		let address = getSomeInfo (getBeginWith json "address: ") "address: " ','
-		let about = getSomeInfo (getBeginWith json "about: ") "about: " ','
-		let registered = getSomeInfo (getBeginWith json "registered: ") "registered: " ','
-		let latitude = getSomeInfo (getBeginWith json "latitude: ") "latitude: " ','
-		let longitude = getSomeInfo (getBeginWith json "longitude: ") "longitude: " ','
-		let friends = createFriends (getSomeInfo (getBeginWith json "friends: [") "friends: [" ']')
-		let greeting = getSomeInfo (getBeginWith json "greeting: ") "greeting: " ','
-		let favoriteFruit = getSomeInfo (getBeginWith json "favoriteFruit: ") "favoriteFruit: " ','
-		createUser id index isActive balance picture age eyeColor name company email phone address about registered latitude longitude friends greeting favoriteFruit	
+    do
+        let id = getSomeInfo (getBeginWith json "_id:") "_id: " ','
+        let index = read (getSomeInfo (getBeginWith json "index: ") "index: " ',') :: Int
+        let isActive = read (capitalFst (getSomeInfo (getBeginWith json "isActive: ") "isActive: " ',')) :: Bool
+        let balance = getSomeInfo (getBeginWith json "balance: ") "balance: " ','
+        let picture = getSomeInfo (getBeginWith json "picture: ") "picture: " ','
+        let age = read (getSomeInfo (getBeginWith json "age: ") "age: " ',') :: Int
+        let eyeColor = getSomeInfo (getBeginWith json "eyeColor: ") "eyeColor: " ','
+        let name = createName (getSomeInfo (getBeginWith json "first: ") "first: " ',') (getSomeInfo (getBeginWith json "last: ") "last: " ',')
+        let company = getSomeInfo (getBeginWith json "company: ") "company: " ','
+        let email = getSomeInfo (getBeginWith json "email: ") "email: " ','
+        let phone = getSomeInfo (getBeginWith json "phone: ") "phone: " ','
+        let address = getSomeInfo (getBeginWith json "address: ") "address: " ','
+        let about = getSomeInfo (getBeginWith json "about: ") "about: " ','
+        let registered = getSomeInfo (getBeginWith json "registered: ") "registered: " ','
+        let latitude = getSomeInfo (getBeginWith json "latitude: ") "latitude: " ','
+        let longitude = getSomeInfo (getBeginWith json "longitude: ") "longitude: " ','
+        let friends = createFriends (getSomeInfo (getBeginWith json "friends: [") "friends: [" ']')
+        let greeting = getSomeInfo (getBeginWith json "greeting: ") "greeting: " ','
+        let favoriteFruit = getSomeInfo (getBeginWith json "favoriteFruit: ") "favoriteFruit: " ','
+        createUser id index isActive balance picture age eyeColor name company email phone address about registered latitude longitude friends greeting favoriteFruit   
 
 haskellToXml::[User]->String
 haskellToXml [] = ""
 haskellToXml (x:xs) = "<User>\n"++
-						"<id>" ++ _id x ++ "</id>\n"++
-						"<name> <first>" ++ getFirstName (name x) ++ "</first>"++"<last>" ++ getLastName (name x)++ "</last> </name>\n"++
-						"<index>"++ show (index x) ++ "</index>\n"++
-						"<isActive>"++ show (isActive x) ++ "</isActive>\n"++
-						"<balance>"++ balance x ++ "</balance>\n"++
-						"<picture>"++ picture x ++ "</picture>\n"++
-						"<age>"++ show (age x) ++ "</age>\n"++
-						"<eyeColor>"++ eyeColor x ++ "</eyeColor>\n"++
-						"<company>"++ company x ++ "</company>\n"++
-						"<email>"++ email x ++ "</email>\n"++
-						"<phone>"++ phone x ++ "</phone>\n"++
-						"<address>"++ address x ++ "</address>\n"++
-						"<registered>"++ registered x ++ "</registered>\n"++
-						"<latitude>"++ latitude x ++ "</latitude>\n"++
-						"<longitude>"++ longitude x ++ "</longitude>\n"++
-						"<friends>"++ getFriends (friends x) ++ "</friends>\n"++
-						"<greeting>"++ greeting x ++ "</greeting>\n"++
-						"<favoriteFruit>"++ favoriteFruit x ++ "</favoriteFruit>\n"++
-					  "</User>" ++ haskellToXml xs
+                        "\t<id>" ++ _id x ++ "</id>\n"++
+                        "\t<name> \n\t\t<first>" ++ getFirstName (name x) ++ "</first>\n"++"\t\t<last>" ++ getLastName (name x)++ "</last>\n\r\t</name>\n"++
+                        "\t<index>"++ show (index x) ++ "</index>\n"++
+                        "\t<isActive>"++ show (isActive x) ++ "</isActive>\n"++
+                        "\t<balance>"++ balance x ++ "</balance>\n"++
+                        "\t<picture>"++ picture x ++ "</picture>\n"++
+                        "\t<age>"++ show (age x) ++ "</age>\n"++
+                        "\t<eyeColor>"++ eyeColor x ++ "</eyeColor>\n"++
+                        "\t<company>"++ company x ++ "</company>\n"++
+                        "\t<email>"++ email x ++ "</email>\n"++
+                        "\t<phone>"++ phone x ++ "</phone>\n"++
+                        "\t<address>"++ address x ++ "</address>\n"++
+                        "\t<registered>"++ registered x ++ "</registered>\n"++
+                        "\t<latitude>"++ latitude x ++ "</latitude>\n"++
+                        "\t<longitude>"++ longitude x ++ "</longitude>\n"++
+                        "\t<friends>\n"++ getFriends (friends x) ++ "\t</friends>\n"++
+                        "\t<greeting>"++ greeting x ++ "</greeting>\n"++
+                        "\t<favoriteFruit>"++ favoriteFruit x ++ "</favoriteFruit>\n"++
+                      "\r</User>\n" ++ haskellToXml xs
